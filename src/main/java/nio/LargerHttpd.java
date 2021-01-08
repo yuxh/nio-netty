2b18eb356cbf3c2f2d902dff4534913ee02257e8
yuxiaohui <@>
2021-01-08 19:17:39 +0800

    注释

--- src/main/java/nio/LargerHttpd.java
+++ src/main/java/nio/LargerHttpd.java
@@ -9,11 +9,22 @@ import java.nio.channels.*;
 import java.nio.charset.*;
 import java.util.regex.*;
 
+/**
+ *  accept new client socket connections, wrap them in an instance
+ * of HttpdConnection, and then watch the client’s status with a Selector.
+ */
 public class LargerHttpd {
     Selector clientSelector;
 
     public void run(int port, int threads) throws IOException {
         clientSelector = Selector.open();
+        //类似建立普通的ServerSocket，except that we must
+        //first create an InetSocketAddress object to hold the local loopback address and port
+        //combination of our server socket and then explicitly bind our socket to that address
+        //with the ServerSocketChannel bind() method. We also configure the server socket
+        //to nonblocking mode and register it with our main Selector so that we can select for
+        //client connections in the same loop that we use to select for client read and write
+        //readiness.
         ServerSocketChannel ssc = ServerSocketChannel.open();
         ssc.configureBlocking(false);
         InetSocketAddress sa = new InetSocketAddress(InetAddress.getLoopbackAddress(), port);
@@ -41,7 +52,7 @@ public class LargerHttpd {
 
                         //有个误区，如果key的OP_READ已经存在，取消监听，isReadable仍然是true（因为针对的是readyOps）
                         key.interestOps(0);
-
+//一旦检测到客户端准备发送或接收数据，就递交一个 Runnable task给Executor。这个task根据就绪的操作，在对应的客户端上调用 read() 或 write()。
                         executor.execute(new Runnable() {
                             public void run() {
                                 try {
@@ -65,6 +76,7 @@ public class LargerHttpd {
         clientSocket.configureBlocking(false);
         SelectionKey key = clientSocket.register(clientSelector,
                 SelectionKey.OP_READ);
+        //FIXME 是否意味着 一个socket 对应 一个key 对应一个channel 对应一个HttpdConnection？
         HttpdConnection client = new HttpdConnection(clientSocket);
         key.attach(client);
     }
@@ -85,7 +97,9 @@ public class LargerHttpd {
         new LargerHttpd().run(1235, 3/*threads*/);
     }
 }
-
+/**
+ encapsulates a socket and handles the conversation with the client
+ */
 class HttpdConnection {
     static Charset charset = Charset.forName("8859_1");
     static Pattern httpGetPattern = Pattern.compile("(?s)GET /?(\\S*).*");
@@ -99,12 +113,13 @@ class HttpdConnection {
     HttpdConnection(SocketChannel clientSocket) {
         this.clientSocket = clientSocket;
     }
-
+//Each read gets as much data as available and checks
+//to see whether we’ve reached the end of a line (a \n newline character).
     void read(SelectionKey key) throws IOException {
         if (request == null && (clientSocket.read(buff) == -1
                 || buff.get(buff.position() - 1) == '\n'))
             processRequest(key);
-        else
+        else//On each incomplete call to read(), we set the interest ops  of our key back to OP_READ
             key.interestOps(SelectionKey.OP_READ);
     }
 
@@ -131,6 +146,7 @@ class HttpdConnection {
                     CharBuffer.wrap(response), buff, true);
             buff.flip();
         }
+        //完成了读和处理请求，准备发送响应
         key.interestOps(SelectionKey.OP_WRITE);
     }
 
@@ -149,6 +165,9 @@ class HttpdConnection {
             } else
                 filePosition += sent;
         }
+        //When we’re done, we close the client
+        //socket and cancel our key, which causes it to be removed from the Selector’s key set
+        //during the next select operation (discarding our HttpdConnection object with it).
         if (response == null && file == null) {
             clientSocket.close();
             key.cancel();
